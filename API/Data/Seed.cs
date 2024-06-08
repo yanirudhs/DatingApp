6ae6293c29bd11f1b976ecd995afb796671f2251
Anirudh Yendamuri <yanirudhs@outlook.com>
2024-06-08 06:29:04 +0530

    Adding DTOs and Extending API

---
+++ API/Data/Seed.cs
@@ -0,0 +1,36 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class Seed
+{
+    public static async Task SeedUsers(DataContext dataContext)
+    {
+        if(await dataContext.Users.AnyAsync()) return;
+
+        var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+
+        var options = new JsonSerializerOptions{ PropertyNameCaseInsensitive = true };
+
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
+
+        foreach (var user in users)
+        {
+            using var hmac = new HMACSHA512();
+
+            user.UserName = user.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+            user.PasswordSalt = hmac.Key;
+
+            dataContext.Users.Add(user);
+        }
+
+        await dataContext.SaveChangesAsync();
+    }
+}
+
+
